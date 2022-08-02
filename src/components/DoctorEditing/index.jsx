27d068edcda3d5d6960445b530f6abd9f69ebd07
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-08-03 00:42:48 +0300

    fix selected doctor

--- src/components/DoctorEditing/index.jsx
+++ src/components/DoctorEditing/index.jsx
@@ -1,32 +1,14 @@
-import { useFormik } from 'formik'
+import { memo } from 'react'
 //
-import Switch from "react-switch";
+import Select from 'react-select';
 //
-import DatePicker from "react-datepicker";
-//
-import routes from '../../routes';
-import config from '../../config';
+import DoctorEditingForm from '../DoctorEditingForm';
 //
 import './DoctorEditing.css'
 
 
-const DoctorEditing = () => {
+const DoctorEditing = ({ selectedDoctor, selectedDoctorOption, onChangeSelectedDoctorOption }) => {
 
-    const formik = useFormik({
-        initialValues: {
-            doctor_photo: '',
-            doctor_spec: '',
-            is_locked: false,
-            description: '',
-            priority_until: new Date
-        },
-        onSubmit: values => {
-            config.api_host.post(routes.update_doctor, {
-                values,
-                priority_until: +new Date(values.priority_until) / 1000
-            })
-        }
-    })
 
     return (
         <div className="doctor_editing">
@@ -35,89 +17,21 @@ const DoctorEditing = () => {
                     Редактирования врача
                 </h4>
             </div>
-            <div className='doctor_editing_content'>
-                <form
-                    onSubmit={formik.handleSubmit}
-                    className="doctor_editing_content_form"
-                >
-                    <div className="doctor_editing_content_form_element">
-                        <span className="doctor_editing_content_form_element_name">
-                            Ссылка на фото
-                        </span>
-                        <input
-                            id="doctor_photo"
-                            value={formik.values.doctor_photo}
-                            onChange={formik.handleChange}
-                            placeholder='Ссылка на фото'
-                            className="doctor_editing_content_form_element_input"
-                            type="text"
-                        />
-                    </div>
-                    <div className="doctor_editing_content_form_element">
-                        <span className="doctor_editing_content_form_element_name">
-                            Специализация
-                        </span>
-                        <textarea
-                            id="doctor_spec"
-                            value={formik.values.doctor_spec}
-                            onChange={formik.handleChange}
-                            className="doctor_editing_content_form_element_textarea"
-                            rows={5}
-                            type="text"
-                            placeholder='Специализация'
-                        />
-                    </div>
-                    <div className="doctor_editing_content_form_element">
-                        <span className="doctor_editing_content_form_element_name">
-                            Заблокирован
-                        </span>
-                        <div className="doctor_editing_content_form_element_switch">
-                            <Switch
-                                id="is_locked"
-                                checked={formik.values.is_locked}
-                                onChange={selectedOption => {
-                                    formik.setFieldValue('is_locked', selectedOption)
-                                }}
-                            />
-                        </div>
-                    </div>
-                    <div className="doctor_editing_content_form_element">
-                        <span className="doctor_editing_content_form_element_name">
-                            Описание
-                        </span>
-                        <textarea
-                            id="description"
-                            value={formik.values.description}
-                            onChange={formik.handleChange}
-                            className="doctor_editing_content_form_element_textarea"
-                            rows={5}
-                            type="text"
-                            placeholder='Описание'
-                        />
-                    </div>
-                    <div className="doctor_editing_content_form_element">
-                        <span className="doctor_editing_content_form_element_name">
-                            Приоритет до
-                        </span>
-                        <div style={{ width: '60%' }}>
-                            <DatePicker
-                                id="priority_until"
-                                selected={formik.values.priority_until}
-                                onChange={selectedOption => {
-                                    formik.setFieldValue('priority_until', selectedOption)
-                                }}
-                                placeholder='Приоритет до'
-                                className='doctor_editing_content_form_element_picker'
-                            />
-                        </div>
-                    </div>
-                    <button className='doctor_editing_content_form_submit' type="submit">
-                        Сохранить
-                    </button>
-                </form>
+            <div className='doctor_editing_intermediate'>
+                <Select
+                    placeholder='Выберите врача'
+                    selected={selectedDoctorOption}
+                    onChange={onChangeSelectedDoctorOption}
+                    options={selectedDoctorOption}
+                />
             </div>
+            {selectedDoctor && (
+                <div className='doctor_editing_content'>
+                    <DoctorEditingForm selectedDoctor={selectedDoctor} />
+                </div>
+            )}
         </div>
     )
 }
 
-export default DoctorEditing
+export default memo(DoctorEditing)
--- src/pages/PDoctorEditing/index.jsx
+++ src/pages/PDoctorEditing/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 // components
 import Navbar from '../../components/Navbar'
 import Sidebar from '../../components/Sidebar'
@@ -5,16 +6,46 @@ import DoctorEditing from '../../components/DoctorEditing'
 // layouts
 import PageContainer from '../../Layouts/PageContainer'
 //
+import config from '../../config'
+import routes from '../../routes'
+//
 import './PDoctorEditing.css'
 
 
 const PDoctorEditing = () => {
+
+    const [selectedDoctor, setSelectedDoctor] = useState(null)
+
+    const [selectedDoctorOption, setSelectedDoctorOption] = useState([{value: 1, label: 'Doctor first'}])
+
+
+    useEffect(() => {}, [
+        // get request for doctors all list
+    ])
+
+
+    const getDoctor = id => {
+        config.api_host.get(`${routes.get_doctor}?id=${id}`).then(r => {
+            if (r.status === 200) {
+                setSelectedDoctor(r.data)
+            }
+        })
+    }
+
+
+    const onChangeSelectedDoctorOption = selectedOption => {
+        setSelectedDoctorOption(selectedDoctorOption)
+        getDoctor(selectedOption.value)
+    }
+
+    
+
     return (
         <div className='p_doctor_editing'>
             <Navbar />
             <Sidebar />
             <PageContainer>
-                <DoctorEditing />
+                <DoctorEditing selectedDoctor={selectedDoctor} selectedDoctorOption={selectedDoctorOption} onChangeSelectedDoctorOption={onChangeSelectedDoctorOption} />
             </PageContainer>
         </div>
     )
